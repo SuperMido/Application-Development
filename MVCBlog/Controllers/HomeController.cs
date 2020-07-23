7eeffc99756e838691748dd1556c1bca9e34318a
SuperMido <supermido1996@gmail.com>
2020-07-23 11:59:53 +0700

    Finish question 16

---
+++ MVCBlog/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MVCBlog.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(5).ToList();
+            return View(posts);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+    }
+}
